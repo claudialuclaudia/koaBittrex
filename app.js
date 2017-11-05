9b6d14cc1aa0615d7f2234b31346f7868c454960
Claudia Lu <claudialuclaudia@gmail.com>
2017-11-04 21:02:46 -0400

    koa app that returns a JSON list of currencies by querying Bittrex

---
+++ app.js
@@ -0,0 +1,29 @@
+const json = require('koa-json')
+const Koa = require('koa')
+const app = new Koa()
+
+var res = [];
+
+var bittrex = require('node-bittrex-api');
+bittrex.options({
+  'apikey' : API_KEY,
+  'apisecret' : API_SECRET,
+});
+
+bittrex.getmarkets( function (data, err) {
+   if (err) {
+    return console.error(err);
+  }
+  for( var i in data.result ) {
+      res.push({exchange: 'bittrex', pair: data.result[i].MarketName});
+    }
+});
+
+app.use(json())
+
+app.use((ctx) => {
+//   ctx.body = JSON.stringify({BTC: res});
+  ctx.body = {BTC: res};
+})
+
+app.listen(8080, () => { console.log('goto http://localhost:8080') })
